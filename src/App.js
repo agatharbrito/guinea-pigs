97cc74ccd044ec94a6c29f22290b081a864114af
agatharbrito <agatharodrigues234@gmail.com>
2024-04-13 17:45:38 -0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./assets/guineapig.jpeg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Tudo sobre como criar Porquinhos da Índia.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
